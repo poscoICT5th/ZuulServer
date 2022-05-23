8dd32893ba823b3a7afc2ef9aab8651ec147f00e
yeop <yeopribo26@gmail.com>
2022-05-23 16:36:21 +0900

    war 추가

--- src/main/java/Pack01/ZuulServerApplication.java
+++ src/main/java/Pack01/ZuulServerApplication.java
@@ -2,6 +2,8 @@ package Pack01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
@@ -10,10 +12,14 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableEurekaClient
 @EnableZuulProxy
 @SpringBootApplication
-public class ZuulServerApplication {
-
+public class ZuulServerApplication extends SpringBootServletInitializer{
+	// 이 부분을 추가함 (war 배포를 위한 소스)
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ZuulServerApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulServerApplication.class, args);
 	}
-
 }
